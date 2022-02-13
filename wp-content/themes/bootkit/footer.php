2b30648332ff4583d786c9002f41686b3cd4aae5
Lena Belevtsova <lenabelevtsova7@gmail.com>
2022-02-13 10:02:50 +0200

    first commit

---
+++ wp-content/themes/bootkit/footer.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white"> <?php echo get_theme_mod('bootkit_footer_copyright_text'); ?>2022<br></p>
+    </div>
+    
+    <!-- /.container -->
+</footer>
+
+<?php wp_footer();?>
+
+</body>
+
+</html>
